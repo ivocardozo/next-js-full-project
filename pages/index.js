4641d25f0d368599cbda3fe8ac97bcd62084c0ef
icarvar <ivo.cardozo@preservica.com>
2023-01-14 04:01:16 -0400

    adding the 'Hero' component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import Hero from '../components/home-page/hero'
 function HomePage() {
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+        <Hero/>
+
+        
+    </Fragment>
   )
 }
 
